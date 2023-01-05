f795d43ca2509af2a571238312ba140fbcaf1c0a
Олег Ларионов <m2008656@edu.misis.ru>
2023-01-05 16:00:02 +0300

    lame fetch

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div><button @click="getWeather">dai pogodu</button></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      weater: null
+    }
+  },
+  methods: {
+    async getWeather() {
+      const api_key = 'f4ca3fbb5a33c8e080b9f86a603a5926'
+      const f = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api_key}`);
+      const data = await f.json();
+      console.log(data)
+    }
   }
 }
 </script>
@@ -23,4 +33,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body, html {
+  height: 100%;
+}
+
+body {
+  /* The image used */
+  background-image: url('https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlc3xlbnwwfHwwfHw%3D&w=1000&q=80');
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+
 </style>
